caaaa99683795f0d4db5b00b46bcdb8af113c76e
Adrian Ko <adrianchko@gmail.com>
2015-01-09 22:06:47 +0000

    Pattern matching

--- src/L2BasicsContinued.scala
+++ src/L2BasicsContinued.scala
@@ -6,6 +6,24 @@ object L2BasicsContinued {
     addOne(1)
     val plusOne = new AddOne()
     plusOne(1)
+
+    //pattern matching
+    val times = 1
+    times match {
+      case 1 => "one"
+      case 2 => "two"
+      case _ => "some other number"
+    }
+  }
+
+  def bigger(o: Any): Any = {
+    o match {
+      case i: Int if i < 0 => i - 1
+      case i: Int => i + 1
+      case d: Double if d < 0.0 => d - 0.1
+      case d: Double => d + 0.1
+      case text: String => text + "s"
+    }
   }
 }
 
