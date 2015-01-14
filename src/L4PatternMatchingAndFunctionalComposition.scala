0dc78e2e550c8f8a3de3c99c9e1cac8b2a63f3eb
Adrian Ko <adrianchko@gmail.com>
2015-01-14 21:31:41 +0000

    print g

--- src/L4PatternMatchingAndFunctionalComposition.scala
+++ src/L4PatternMatchingAndFunctionalComposition.scala
@@ -2,7 +2,7 @@ object L4PatternMatchingAndFunctionalComposition {
     def main(args: Array[String]) {
         //function composition
         def f(s: String) = "f(" + s + ")"
-        def g(s: String) = "f(" + s + ")"
+        def g(s: String) = "g(" + s + ")"
 
         //compose
         val fComposeG = (f _).compose(g _)
