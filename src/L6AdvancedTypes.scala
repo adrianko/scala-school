cac62d24ea0bd727973f2517686578ef2cb84035
Adrian Ko <adrianchko@gmail.com>
2015-01-14 22:32:58 +0000

    type classes

--- src/L6AdvancedTypes.scala
+++ src/L6AdvancedTypes.scala
@@ -1,5 +1,8 @@
 object L6AdvancedTypes {
     def main(args: Array[String]) {
-        
+        //type classes
+        implicit def strToInt(x: String) = x.toInt
+
+        class Container[A <% Int] { def addIt(x: A) = 123 + x}
     }
 }
