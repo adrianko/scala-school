8e4b979e2a1e4011efc93342b99c1e0ed02f5f34
Adrian Ko <adrianchko@gmail.com>
2015-01-14 21:45:06 +0000

    parametric polymorphism

--- src/L5TypeBasics.scala
+++ src/L5TypeBasics.scala
@@ -1,5 +1,10 @@
 object L5TypeBasics {
     def main(args: Array[String]) {
+        //parametric polymorphism
+        val l = List(2, 1, "bar", "foo", Nil)
+        println(l.head)
 
+        def drop1[A](l: List[A]) = l.tail
+        println(drop1(List(1,2,3)))
     }
 }
